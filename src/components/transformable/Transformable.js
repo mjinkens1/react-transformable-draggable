4dd5a4c679479789333f3da88201f95659810d09
Matt Jinkens <matt@pixelandtexel.com>
2019-08-23 11:39:55 -0500

    Debug changes

--- src/components/transformable/Transformable.js
+++ src/components/transformable/Transformable.js
@@ -94,7 +94,7 @@ const TransformableCore = ({
     const getMinHeight = () => (lockAspectRatio ? minWidth / aspectRatio : minHeight)
 
     const getOpacity = (isDragging, isDragLayer) => {
-        console.log(isDragging)
+        console.log()
 
         if (isDragLayer) {
             return dragLayerIsDragging ? 1 : 0
