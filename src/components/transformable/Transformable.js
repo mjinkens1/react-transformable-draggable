53b5f730e0e79e382c27058502c214800dfc6ce4
Matt Jinkens <matt@pixelandtexel.com>
2019-08-23 11:44:46 -0500

    Temp bug fix

--- src/components/transformable/Transformable.js
+++ src/components/transformable/Transformable.js
@@ -94,7 +94,7 @@ const TransformableCore = ({
     const getMinHeight = () => (lockAspectRatio ? minWidth / aspectRatio : minHeight)
 
     const getOpacity = (isDragging, isDragLayer) => {
-        // console.log()
+        console.log()
 
         if (isDragLayer) {
             return dragLayerIsDragging ? 1 : 0
