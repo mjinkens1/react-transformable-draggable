2e5dc8e7ac81c4b9373116e9e63539f1da6526e0
Matt <matthew.jinkens@gmail.com>
2020-04-26 18:39:51 -0500

    Update bundle analyzer config

--- webpack.config.js
+++ webpack.config.js
@@ -78,7 +78,7 @@ module.exports = {
         extensions: ['.js'],
     },
     plugins: [
-        new BundleAnalyzerPlugin(),
+        new BundleAnalyzerPlugin({ analyzerMode: 'static' }),
         new CleanWebpackPlugin(),
         new CircularDependencyPlugin({
             exclude: /node_modules/,
